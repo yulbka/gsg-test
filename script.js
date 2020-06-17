0ab75bc872d85535ff16407db2251450f0ef2cbd
Yuliya Ramanouskaya <yulbka13@mail.ru>
2020-06-17 21:01:41 +0300

    feat: finish task

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ pagination.addEventListener('click', (event) => {
   target.classList.add('dot_active');
   slides.style.transform = `translateX(${-Number(target.dataset.index - 1) * sliderWidth - 2}px)`;
 });
+
+// F2 notification
+
+const popup = document.querySelector('.popup');
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();  
+  popup.classList.add('popup_active');
+});
+
+const popupBtn = document.querySelector('.popup__button');
+popupBtn.addEventListener('click', () => {
+  console.log('ok');
+  popup.classList.remove('popup_active');
+});
